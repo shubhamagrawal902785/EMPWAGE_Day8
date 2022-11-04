1595eedb2cede0300c539531f2c976d39c1d6332
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-04 21:08:54 +0530

    welcome the employee wage

---
+++ src/com/bridgelabz/day8/Empwage.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day8;
+
+public class Empwage {
+	public static void main(String args[])
+	{
+		int a = (int)(Math.random() * 2);
+		if(a==1)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
+
+}
